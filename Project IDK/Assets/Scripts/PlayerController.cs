88d366d8b4f479e466f7cdfd632caf55a5aa2ce6
DevNatrix <natrixfn@gmail.com>
2023-11-07 11:30:36 +0100

    PlayerController

---
+++ Project IDK/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        
+    }
+}
